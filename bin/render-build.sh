04be41da479165621d8450750f00a1269e27cd6a
Uotani SAtoshi <satoshi_uotani@spice-factory.co.jp>
2025-06-26 16:42:34 +0900

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,13 @@
 
 set -o errexit
 
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install npm packages
+npm install
+
 bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
