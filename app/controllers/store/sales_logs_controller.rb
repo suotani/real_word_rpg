50be23b6ae4a5eec553f2a8d10e074344d4cedff
satoshi_uotani <satoshi_uotani@c.dwango.co.jp>
2026-07-01 14:21:20 +0900

    売り上げ記録修正

--- app/controllers/store/sales_logs_controller.rb
+++ app/controllers/store/sales_logs_controller.rb
@@ -1,5 +1,13 @@
 class Store::SalesLogsController < Store::ApplicationController
   def index
-    @sales_logs = current_user.sales_logs.order(target_date: :desc)
+    year  = params[:year].presence&.to_i  || Date.current.year
+    month = params[:month].presence&.to_i || Date.current.month
+
+    @year  = year
+    @month = month
+
+    @sales_logs = current_user.sales_logs
+                              .where(target_date: Date.new(year, month, 1)..Date.new(year, month, -1))
+                              .order(target_date: :desc)
   end
 end
